76fc1f5e3306620d10a8a40f2dc398fdbcc7410d
qyd9 <96965248+qyd9@users.noreply.github.com>
2022-01-02 11:10:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=97df801c-65d1-4a12-9d64-e55f58e71d0c
+AUUID=78df801c-65d1-4a12-9d64-e55f58e71d0c
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/evgyu/railwayxray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/evgyu/railwayxray/master/etc/xray.json
